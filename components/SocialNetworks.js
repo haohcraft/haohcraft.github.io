8d7db22702e2421609dec87e2f17c0b7bf4e5ada
Hao Huang <hhuang@dataminr.com>
2016-06-15 00:44:24 -0400

    social medias

--- components/SocialNetworks.js
+++ components/SocialNetworks.js
@@ -3,18 +3,19 @@ import { rhythm } from 'utils/typography'
 import TwitterIcon from 'react-icons/lib/fa/twitter'
 import GithubIcon from 'react-icons/lib/fa/github'
 import InstagramIcon from 'react-icons/lib/fa/instagram'
-import SpotifyIcon from 'react-icons/lib/fa/spotify'
+import LinkedinIcon from 'react-icons/lib/fa/linkedin'
 
 const SocialNetworks = (props) => {
   const marginBetweenIcons = rhythm(1/6)
   return (
-    <ul className="social-networks"
+    <div className="SocialNetworks"
       style={ props.style }
     >
-    <li style={{ margin: marginBetweenIcons }}><a href="http://twitter.com/haohmobile" title="Twitter"><TwitterIcon /></a></li>
-    <li style={{ margin: marginBetweenIcons }}><a href="http://instagram.com/haohmobile" title="instagram"><InstagramIcon /></a></li>
-    <li style={{ margin: marginBetweenIcons }}><a href="http://github.com/haohcraft" title="github"><GithubIcon /></a></li>
-    </ul>
+    <li style={{ margin: marginBetweenIcons }}><a href="http://twitter.com/haohmobile" title="Twitter" target="_blank"><TwitterIcon /></a></li>
+    <li style={{ margin: marginBetweenIcons }}><a href="http://instagram.com/haohmobile" title="instagram" target="_blank"><InstagramIcon /></a></li>
+    <li style={{ margin: marginBetweenIcons }}><a href="http://github.com/haohcraft" title="github" target="_blank"><GithubIcon /></a></li>
+    <li style={{ margin: marginBetweenIcons }}><a href="https://www.linkedin.com/in/haohjob" title="github" target="_blank"><LinkedinIcon /></a></li>
+    </div>
   )
 }
 
--- components/footer/author.js
+++ components/footer/author.js
@@ -2,23 +2,39 @@ import React, {Component} from 'react'
 import { prefixLink } from 'gatsby-helpers';
 import { rhythm } from 'utils/typography';
 import { config } from 'config';
+import SocialNetworks from '../SocialNetworks';
+
+
 export default class Author extends Component {
+    constructor() {
+        super();
+        this.cn = "Author";
+    }
     render() {
+        const style = {
+            display: 'flex',
+        };
+        const styleProfile = {
+            marginRight: rhythm(1/4),
+            marginBottom: 0,
+            width: rhythm(5),
+            height: rhythm(5),
+            borderRadius: '3px'
+        };
+        const styleBio = {
+        };
         return (
-            <p>
+            <div className={`${this.cn}`} style={style}>
+                <div className={`${this.cn}-profile`} style={styleProfile}>
                 <a target="_blank" href="https://www.linkedin.com/in/haohjob">
-                    <img src={prefixLink('/assets/images/base/profile.jpg')}
-                        style={{
-                            float: 'left',
-                            marginRight: rhythm(1/4),
-                            marginBottom: 0,
-                            width: rhythm(2),
-                            height: rhythm(2),
-                            borderRadius: '3px'
-                        }} />
+                    <img src={prefixLink('/assets/images/base/profile.jpg')}/>
                 </a>
+                </div>
+                <div className={`${this.cn}-bio`} style={styleBio}>
                 <strong>{config.authorName}</strong> lives in New York City and currently works as a Front End Engineer at <a href="https://www.dataminr.com/" target="_blank">Dataminr</a> to write abstracts with React/Redux.
-            </p>
+                <SocialNetworks />
+                </div>
+            </div>
         );
     }
 }
