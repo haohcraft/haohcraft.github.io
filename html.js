c27476a4c89f729570661802af18898d5864c9f0
haohcraft <hao.huang@datadoghq.com>
2016-10-16 17:14:44 -0400

    added favion

--- html.js
+++ html.js
@@ -26,6 +26,7 @@ module.exports = React.createClass({
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="icon" type="image/x-icon" href={prefixLink('/assets/images/base/profile.jpg')} />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
